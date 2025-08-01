d8879d9bb1a785a50d3244722c8cffd6088dfaae
Maverdale <renhardjulianusbernard@gmail.com>
2025-08-01 14:09:33 +0700

    Website
    Beta 1.0.0

---
+++ settings.gradle.kts
@@ -0,0 +1,37 @@
+import java.net.URI
+
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        maven {
+            url = URI("https://jitpack.io")
+        }
+        // Corrected and simplified Mapbox repository
+        maven {
+            name = "Mapbox"
+            url = URI("https://api.mapbox.com/downloads/v2/releases/maven")
+            credentials {
+                username = "mapbox"
+                password = providers.gradleProperty("MAPBOX_DOWNLOADS_TOKEN").get()
+            }
+        }
+    }
+}
+
+rootProject.name = "MyPinPoin"
+include(":app")
